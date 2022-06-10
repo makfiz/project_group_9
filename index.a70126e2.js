6668f51d3b7c0ca18fde7a321533003311ee3cf8
makfiz <99556605+makfiz@users.noreply.github.com>
2022-06-10 19:18:07 +0000

    Deploying to gh-pages from @ makfiz/project_group_9@e6d466784226721a280b545492878099120d691f 🚀

---
+++ index.a70126e2.js
@@ -0,0 +1,2 @@
+(()=>{const e={openFranchiseBtn:document.querySelector("[data-franchise-open]"),closeFranchiseBtn:document.querySelector(".franchise-close-btn"),franchiseModal:document.querySelector(".franchise-backdrop"),body:document.querySelector("body")};function c(){e.franchiseModal.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openFranchiseBtn.addEventListener("click",c),e.closeFranchiseBtn.addEventListener("click",c)})();
+//# sourceMappingURL=index.a70126e2.js.map
