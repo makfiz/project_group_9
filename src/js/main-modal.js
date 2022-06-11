9103c5d8960292b922aec6a2b4ff4641e1b66068
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-06-11 14:38:18 +0300

    js file connected
    Підключив js до модалки

--- src/js/main-modal.js
+++ src/js/main-modal.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openMobModalBtn: document.querySelector('[data-mobmodal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     scroll: document.querySelector('[data-scroll'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openMobModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   refs.openModalBtn.addEventListener('click', toggleScroll);
