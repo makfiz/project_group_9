66f6fcf01dd284ba93436755e18b285aaa4c108d
Maksym Hlanskov <makfiz@gmail.com>
2022-06-12 11:57:52 +0300

    fix
    fix mob menu
    добавил скрипт скрывающий кнопки в хедере при открытии меню

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.mob-menu');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const hideHeaderBtn = document.querySelector('.header-btn');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -9,6 +10,7 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
     openMenuBtn.classList.toggle('is-hidden');
+    hideHeaderBtn.classList.toggle('is-hidden');
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -16,10 +18,9 @@
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
+  // window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  //   if (!e.matches) return;
+  //   mobileMenu.classList.remove('is-open');
+  //   openMenuBtn.setAttribute('aria-expanded', false);
+  // });
 })();
