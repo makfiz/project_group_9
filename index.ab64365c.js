1a18c3c436e8268c30e5e8aec2656e0b9fe2ca12
makfiz <99556605+makfiz@users.noreply.github.com>
2022-06-12 08:59:03 +0000

    Deploying to gh-pages from @ makfiz/project_group_9@9c911896be8e4776561e0e147f59099751042254 🚀

---
+++ index.ab64365c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".mob-menu"),t=document.querySelector(".js-open-menu"),n=document.querySelector(".js-close-menu"),c=document.querySelector(".header-btn"),i=function(){var n="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open"),t.classList.toggle("is-hidden"),c.classList.toggle("is-hidden")};t.addEventListener("click",i),n.addEventListener("click",i)}();
+//# sourceMappingURL=index.ab64365c.js.map
