84b54ac763a23683b9048c221b6fe9f5d70c24dd
makfiz <99556605+makfiz@users.noreply.github.com>
2022-06-12 10:56:28 +0000

    Deploying to gh-pages from @ makfiz/project_group_9@52c5a3f0d07d8cfeb21475ce5f1c6402df408ff0 🚀

---
+++ index.272e97a6.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-open]"),openMobModalBtn:document.querySelector("[data-mobmodal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-transparent"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.openMobModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.272e97a6.js.map
--- index.e6f927f0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openMobModalBtn:document.querySelector("[data-mobmodal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-transparent")}e.openModalBtn.addEventListener("click",o),e.openMobModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.e6f927f0.js.map
