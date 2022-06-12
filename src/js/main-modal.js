d9f79dcc153f83acad24c3e0d4a4d1e2f5c543c5
Maksym Hlanskov <makfiz@gmail.com>
2022-06-12 13:42:15 +0300

    Update main-modal.js
    js scrip
    scroll fix

--- src/js/main-modal.js
+++ src/js/main-modal.js
@@ -4,20 +4,15 @@
     openMobModalBtn: document.querySelector('[data-mobmodal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    // scroll: document.querySelector('[data-scroll'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openMobModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  // refs.openModalBtn.addEventListener('click', toggleScroll);
-  // refs.closeModalBtn.addEventListener('click', toggleScroll);
-
   function toggleModal() {
     refs.modal.classList.toggle('is-transparent');
+    refs.body.classList.toggle('no-scroll');
   }
-  // function toggleScroll() {
-  //   refs.scroll.classList.toggle('no-scroll');
-  // }
 })();
